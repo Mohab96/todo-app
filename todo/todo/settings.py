5a074a68bb8b1592af0deacf77a2a1c5fb07c59f
Mohab Yaser <mohabkhaleefa540@gmail.com>
2024-03-14 19:08:55 +0200

    Bug fixes

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -183,5 +183,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mohabyaserofficial2003@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ju$td0tHeStufF'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
